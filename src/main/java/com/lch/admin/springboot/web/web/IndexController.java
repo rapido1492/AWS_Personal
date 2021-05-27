d74425cb74835d1365407e6124f27ef007a68f0d
DESKTOP-EUNPBV1\lch <rain1492@monsternplay.com>
2021-05-27 19:00:12 +0900

    index페이지 추가

---
+++ src/main/java/com/lch/admin/springboot/web/web/IndexController.java
@@ -0,0 +1,12 @@
+package com.lch.admin.springboot.web.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/index/")
+    public String index() {
+        return "index";
+    }
+}
--- src/test/java/com/lch/admin/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/lch/admin/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,5 +44,23 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        LocalDateTime now = LocalDateTime.of(2021,5,27,18,28,0);
+        postsRepository.save(Posts.builder()
+        .title("title")
+        .content("content")
+        .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate = " + posts.getCreateDate() + ", modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate().isAfter(now));
+        assertThat(posts.getModifiedDate().isAfter(now));
     }
 }
