dba3109ce0070fde6fac2d2acd7393b5c34cc2c0
rapido1492 <rapido1492@gmail.com>
2021-05-27 21:57:27 +0900

    mustache 수정

--- src/test/java/com/lch/admin/springboot/web/IndexControllerTest.java
+++ src/test/java/com/lch/admin/springboot/web/IndexControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,7 +18,7 @@ public class IndexControllerTest {
 
     @Test
     public void 메인페이지_로딩() {
-        String body = this.restTemplate.getForObject("/index/", String.class);
+        String body = this.restTemplate.getForObject("/", String.class);
 
         assertThat(body).contains("rain1492 웹 서비스");
     }
