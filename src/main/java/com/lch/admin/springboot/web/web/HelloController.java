60784a1f848757b8a60d3b6c05cc0e7126314bb7
rapido1492 <rapido1492@gmail.com>
2021-05-27 22:06:23 +0900

    test 삭제

--- src/main/java/com/lch/admin/springboot/web/web/HelloController.java
+++ src/main/java/com/lch/admin/springboot/web/web/HelloController.java
@@ -14,7 +14,6 @@ public class HelloController {
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
         return new HelloResponseDto(name, amount);
-        //sdfs
     }
 
 }
