a636abc7a7630d2b525ee430f44770cbecf158e0
DESKTOP-EUNPBV1\lch <rain1492@monsternplay.com>
2021-05-27 16:46:54 +0900

    추가

--- src/main/java/com/lch/admin/springboot/web/service/posts/PostsService.java
+++ src/main/java/com/lch/admin/springboot/web/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.lch.admin.springboot.web.service.posts;
 
+import com.lch.admin.springboot.web.domain.posts.Posts;
 import com.lch.admin.springboot.web.domain.posts.PostsRepository;
+import com.lch.admin.springboot.web.web.dto.PostsResponseDto;
 import com.lch.admin.springboot.web.web.dto.PostsSaveRequestDto;
+import com.lch.admin.springboot.web.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
